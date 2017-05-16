7872c483fa34e2f9d8346906c5947daf15e66c75
simsinght <simsinght@gmail.com>
2017-05-15 20:17:24 -0700

    some stuff

--- Fooider/Fooider/MyKolodaViewController.swift
+++ Fooider/Fooider/MyKolodaViewController.swift
@@ -8,27 +8,62 @@
 
 import UIKit
 import Koloda
+import CoreLocation
 
-class MyKolodaViewController: UIViewController {
+class MyKolodaViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
     @IBOutlet weak var kolodaView: KolodaView!
     
     var images: [UIImage] = []
     
+    var currLocation:CLLocationCoordinate2D!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        /* location */
+        // Ask for Authorisation from the User.
+        self.locationManager.requestAlwaysAuthorization()
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+            getFoodsRestaurants();
+        }
+        
+        /* kolodaView */
         // Do any additional setup after loading the view.
         images.append(#imageLiteral(resourceName: "ryan"))
         kolodaView.dataSource = self
         kolodaView.delegate = self
     }
+    
+    func getFoodsRestaurants() {
+        
+    }
 
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        self.currLocation = locValue
+        // print("locations = \(locValue.latitude) \(locValue.longitude)")
+        // locations = 37.785834 -122.406417
+    }
+
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func onButtonPrint(_ sender: Any) {
+        print("clicked")
+    }
 
     // MARK: - Navigation
 
